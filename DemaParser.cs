78c26984f84af30836236168ff00272559120bb1
Mert Fahri Çakar <mertfcakar@outlook.com>
2025-09-17 13:56:27 +0300

    edit sg fix

--- DemaParser.cs
+++ DemaParser.cs
@@ -11,7 +11,7 @@ namespace DEMA_Parser
         private static readonly Dictionary<string, string> FriendlyNames = new Dictionary<string, string>
         {
             { "Mod", "Operation" }, { "Beh", "Behavior" }, { "Health", "Health Status" }, { "NamPlt", "Name Plate" },
-            { "ProOff", "Protection Off" }, { "Blk", "Block" }, { "ActSG", "Active Setting Group" },
+            { "ProOff", "Protection Off" }, { "Blk", "Block" },
             { "StrVal", "Start value" }, { "StrValMul", "Start value multiplier" }, { "OpDlTmms", "Operate delay time" },
             { "RsDlTmms", "Reset delay time" }, { "TmMult", "Time multiplier" }, { "TmACrv", "Operating curve type" },
             { "TypRsCrv", "Reset curve type" }, { "DirMod", "Directional mode" }, { "TorqueAng", "Torque Angle" },
@@ -27,6 +27,14 @@ namespace DEMA_Parser
             { "TotPF", "Total Power Factor" }, { "A", "Phase Currents" }, { "PPV", "Phase-to-Phase Voltage" },
             { "PNV", "Phase-to-Neutral Voltage" }, { "Hz", "Frequency" },
             { "MemRs", "Reset Memory" }, { "RcdMade", "Record Made" }, { "FltNum", "Fault Number" },
+            // --- SGCB için yeni eklenen isimler ---
+            { "SGCB", "Setting Group Control" },
+            { "ActSG", "Active Setting Group" },
+            { "EditSG", "Edit Setting Group" },
+            { "CnfEdit", "Confirm Edit" },
+            { "NumOfSG", "Number of Setting Groups" },
+            { "LActTm", "Last Activation Time" },
+            { "ResvTms", "Reservation Timeout" }
         };
 
         public static XDocument ConvertDpmToHierarchical(string dpmSclContent)
@@ -43,11 +51,7 @@ namespace DEMA_Parser
             var enumTypes = dpmDoc.Descendants(ns + "EnumType").ToDictionary(x => x.Attribute("id")?.Value ?? "", x => x);
             var daTypes = dpmDoc.Descendants(ns + "DAType").ToDictionary(x => x.Attribute("id")?.Value ?? "", x => x);
 
-            var settingControl = dpmDoc.Descendants(ns + "SettingControl").FirstOrDefault();
-            int numOfSettingGroups = settingControl != null ? int.Parse(settingControl.Attribute("numOfSGs")?.Value ?? "1") : 1;
-
-            string iedName = dpmDoc.Descendants(ns + "IED").FirstOrDefault()?.Attribute("name")?.Value ?? "IED1";
-            string iedPrefix = "A612H04"; // This can be customized if needed for other models.
+            string iedPrefix = "A612H04"; 
 
             foreach (var lDevice in dpmDoc.Descendants(ns + "LDevice"))
             {
@@ -81,8 +85,13 @@ namespace DEMA_Parser
                         string doTypeId = doElement.Attribute("type")?.Value ?? "";
                         if (doTypes.TryGetValue(doTypeId, out var doTypeDef))
                         {
-                            ProcessDataObjectRecursive(ln, doElement, doTypeDef, "", functionBlockGroup, settingsContainer, controlsContainer, statusContainer, measurementsContainer, numOfSettingGroups, doTypes, daTypes, enumTypes, ns, fullLdName);
+                            ProcessDataObjectRecursive(ln, doElement, doTypeDef, "", functionBlockGroup, settingsContainer, controlsContainer, statusContainer, measurementsContainer, doTypes, daTypes, enumTypes, ns, fullLdName);
                         }
+                    }
+                    
+                    if (functionBlockName == "LLN0" && fullLdName == "A612H04PR")
+                    {
+                        AddManualSGCBParameters(settingsContainer, fullLdName, functionBlockName);
                     }
 
                     if (settingsContainer.HasElements) functionBlockGroup.Add(settingsContainer);
@@ -96,7 +105,7 @@ namespace DEMA_Parser
             return outputDoc;
         }
 
-        private static void ProcessDataObjectRecursive(XElement ln, XElement doOrSdoElement, XElement typeDef, string currentPath, XElement functionBlockGroup, XElement settings, XElement controls, XElement status, XElement measurements, int numOfGroups, Dictionary<string, XElement> doTypes, Dictionary<string, XElement> daTypes, Dictionary<string, XElement> enumTypes, XNamespace ns, string fullLdName)
+        private static void ProcessDataObjectRecursive(XElement ln, XElement doOrSdoElement, XElement typeDef, string currentPath, XElement functionBlockGroup, XElement settings, XElement controls, XElement status, XElement measurements, Dictionary<string, XElement> doTypes, Dictionary<string, XElement> daTypes, Dictionary<string, XElement> enumTypes, XNamespace ns, string fullLdName)
         {
             string name = doOrSdoElement.Attribute("name")?.Value ?? "";
             string newPath = string.IsNullOrEmpty(currentPath) ? name : $"{currentPath}.{name}";
@@ -111,19 +120,16 @@ namespace DEMA_Parser
                     case "SE":
                     case "SP": container = settings; break;
                     case "CO": container = controls; break;
-                    //case "ST": container = status; break;
-                    //case "MX": container = measurements; break;
+                    case "ST": container = status; break;
+                    case "MX": container = measurements; break;
                 }
 
                 if (container != null)
                 {
                     if (fc == "SE")
                     {
-                        for (int i = 1; i <= 1; i++) // Using 1 to generate only one setting group for now.
-                        {
-                            var settingGroup = FindOrCreateGroup(functionBlockGroup, new[] { "Settings", $"Setting Group {i}" });
-                            CreateParameter(settingGroup, i, fullLdName, ln, newPath, da, daTypes, enumTypes, ns);
-                        }
+                        var settingGroup = FindOrCreateGroup(functionBlockGroup, new[] { "Settings", "Setting Group 1" });
+                        CreateParameter(settingGroup, 1, fullLdName, ln, newPath, da, daTypes, enumTypes, ns);
                     }
                     else
                     {
@@ -137,7 +143,7 @@ namespace DEMA_Parser
                 string sdoTypeId = sdo.Attribute("type")?.Value ?? "";
                 if (doTypes.TryGetValue(sdoTypeId, out var sdoTypeDef))
                 {
-                    ProcessDataObjectRecursive(ln, sdo, sdoTypeDef, newPath, functionBlockGroup, settings, controls, status, measurements, numOfGroups, doTypes, daTypes, enumTypes, ns, fullLdName);
+                    ProcessDataObjectRecursive(ln, sdo, sdoTypeDef, newPath, functionBlockGroup, settings, controls, status, measurements, doTypes, daTypes, enumTypes, ns, fullLdName);
                 }
             }
         }
@@ -156,15 +162,23 @@ namespace DEMA_Parser
 
             string objectAddress = $"{fullLdName}/{prefix}{lnClass}{lnInst}.{doPath}.{daName}";
             string baseName = doPath.Split('.').First();
-            string friendlyName = FriendlyNames.ContainsKey(baseName) ? FriendlyNames[baseName] : baseName;
-
-            if (doPath.Contains('.'))
+            
+            string friendlyName;
+            if (FriendlyNames.ContainsKey(daName))
             {
-                friendlyName += $" ({doPath.Split('.').Last()}.{daName})";
+                friendlyName = FriendlyNames[daName];
             }
-            else if (daName != "setVal" && daName != "ctlVal" && daName != "general" && daName != "stVal" && daName != "mag")
+            else
             {
-                friendlyName += $" ({daName})";
+                friendlyName = FriendlyNames.ContainsKey(baseName) ? FriendlyNames[baseName] : baseName;
+                if (doPath.Contains('.'))
+                {
+                    friendlyName += $" ({doPath.Split('.').Last()}.{daName})";
+                }
+                else if (daName != "setVal" && daName != "ctlVal" && daName != "general" && daName != "stVal" && daName != "mag")
+                {
+                    friendlyName += $" ({daName})";
+                }
             }
 
             string finalDataType = DetermineCorrectDataType(fc, daName, bType, objectAddress);
@@ -185,12 +199,10 @@ namespace DEMA_Parser
             if (finalDataType == "Enumeration" && !string.IsNullOrEmpty(typeAttr))
             {
                 string finalEnumTypeID = typeAttr;
-
                 if (!enumTypes.ContainsKey(finalEnumTypeID) && daTypes.TryGetValue(finalEnumTypeID, out var daTypeDef))
                 {
                     var bda = daTypeDef.Elements(ns + "BDA")
                                        .FirstOrDefault(b => (b.Attribute("name")?.Value == "ctlVal" || b.Attribute("name")?.Value == "stVal") && b.Attribute("bType")?.Value == "Enum");
-
                     if (bda != null)
                     {
                         finalEnumTypeID = bda.Attribute("type")?.Value ?? finalEnumTypeID;
@@ -221,6 +233,38 @@ namespace DEMA_Parser
             parentElement.Add(parameter);
         }
 
+        /// <summary>
+        /// İstenen SGCB parametrelerini manuel olarak oluşturur ve ilgili gruba ekler.
+        /// </summary>
+        private static void AddManualSGCBParameters(XElement settingsContainer, string fullLdName, string lnName)
+        {
+            var sgcBParameters = new[]
+            {
+                new { Name = "ActSG",   DataType = "Integer", ObjectAddress = $"{fullLdName}/{lnName}.SGCB.ActSG" },
+                new { Name = "CnfEdit", DataType = "Boolean", ObjectAddress = $"{fullLdName}/{lnName}.SGCB.CnfEdit" },
+                new { Name = "EditSG",  DataType = "Integer", ObjectAddress = $"{fullLdName}/{lnName}.SGCB.EditSG" },
+                new { Name = "LActTm",  DataType = "String",  ObjectAddress = $"{fullLdName}/{lnName}.SGCB.LActTm" }, // UTC_TIME -> String
+                new { Name = "NumOfSG", DataType = "Integer", ObjectAddress = $"{fullLdName}/{lnName}.SGCB.NumOfSG" },
+                new { Name = "ResvTms", DataType = "Integer", ObjectAddress = $"{fullLdName}/{lnName}.SGCB.ResvTms" }
+            };
+
+            foreach (var p in sgcBParameters)
+            {
+                var parameter = new XElement("Parameter",
+                    new XElement("Name", FriendlyNames[p.Name]),
+                    new XElement("ObjectAddress", p.ObjectAddress),
+                    new XElement("FunctionalConstraint", "SP"),
+                    new XElement("DataType", p.DataType),
+                    new XElement("Value", ""),
+                    new XElement("MinValue", ""),
+                    new XElement("MaxValue", ""),
+                    new XElement("StepSize", ""),
+                    new XElement("Unit", "")
+                );
+                settingsContainer.Add(parameter);
+            }
+        }
+
         private static bool ParameterRequiresUnit(string baseName)
         {
             var unitlessParameters = new HashSet<string>
@@ -236,28 +280,15 @@ namespace DEMA_Parser
             {
                 if (daName == "Oper")
                 {
-                    if (objectAddress.EndsWith("ActSG.Oper"))
-                    {
-                        return "Integer";
-                    }
-
-                    if (objectAddress.Contains(".Mod.Oper"))
-                    {
-                        return "Enumeration";
-                    }
+                    if (objectAddress.EndsWith("ActSG.Oper")) return "Integer";
+                    if (objectAddress.Contains(".Mod.Oper")) return "Enumeration";
                     return "Boolean";
                 }
                 return GetFriendlyDataType(bType);
             }
-            if ((fc == "SE" || fc == "SP") && daName == "setMag")
-            {
-                return "Float";
-            }
-            if ((fc == "MX" || fc == "ST") && (daName == "mag" || daName == "f" || daName == "instMag"))
-            {
-                return "Float";
-            }
-
+            if ((fc == "SE" || fc == "SP") && daName == "setMag") return "Float";
+            if ((fc == "MX" || fc == "ST") && (daName == "mag" || daName == "f" || daName == "instMag")) return "Float";
+            
             return GetFriendlyDataType(bType);
         }
 
@@ -267,7 +298,6 @@ namespace DEMA_Parser
             {
                 PruneEmptyGroups(child);
             }
-
             if (element.Name == "Group" && !element.HasElements)
             {
                 element.Remove();
